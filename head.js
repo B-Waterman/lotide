45d8af8cb064c60ee4ecfe7ab83825a36c310cdb
B-Waterman <brooke.waterman@outlook.com>
2023-02-06 20:23:08 -0800

    Added additional test assertions for single element arrays and empty arrays. Re-arranged comment structure for clarity.

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-//Determines if two parameters are equal in type and value
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -6,11 +5,15 @@ const assertEqual = function(actual, expected) {
     console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-//Should return first element, or head, of an array
+//Determines if two parameters are equal in type and value
+
 const head = function(array) {
   return array[0];
 };
+//Should return first element, or head, of an array
 
 //Test Assertions
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 5); //Pass!
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //Pass!
+assertEqual(head([1]), 1); //Pass!
+assertEqual(head([]), 15); //Undefined
