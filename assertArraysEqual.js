25abd7c0ed70aecf99d4fa297d23e29f2ca962dd
B-Waterman <brooke.waterman@outlook.com>
2023-02-08 09:54:58 -0800

    Refactored assertArraysEqual function to evaluate whether eqArray returns true.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,7 +20,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
+  if (eqArrays(arr1, arr2) === true) {
     console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`⛔⛔⛔Assertion Failed: ${arr1} !== ${arr2}`);
