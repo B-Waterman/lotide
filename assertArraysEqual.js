abbbf16a9a7b8f0e27d40b5ec0952f166792f721
B-Waterman <brooke.waterman@outlook.com>
2023-02-07 22:06:37 -0800

    Created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(arr1, arr2) {
+  // takes two arrays, if equal = true, if not = false
+  if (arr1.length !== arr2.length) {
+    return false;
+    // checks length first to rule out longer arrays with same starting values
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // if both are arrays, test if arrays are equal
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+      // if elements of arrays are not equal, return false
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
