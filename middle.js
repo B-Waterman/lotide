717d7bf665cc464ba750ffcb7e1d104f54f8829a
B-Waterman <brooke.waterman@outlook.com>
2023-02-10 05:06:56 -0800

    Created middle function, established initial if statement for array with 2 or less elements.

--- middle.js
+++ middle.js
@@ -1,3 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  // takes two arrays, if equal = true, if not = false
+  if (arr1.length !== arr2.length) {
+    return false;
+    // checks length first to rule out longer arrays with same starting values
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // if both are arrays, test if arrays are equal
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+      // if elements of arrays are not equal, return false
+    }
+  }
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -6,3 +27,20 @@ const assertEqual = function(actual, expected) {
   }
   //Template literals pass the assertion's actual values into the final string!
 };
+
+//Scenario 1: total # of elements in array odd #, return middle element
+//Scenario 2: total # of elements in array even #, return two middle elements
+//Scenario 3: array with only one or two elements total, return empty array
+const middle = function(array) {
+  let newArray = []
+  
+};
+
+//Test Cases:
+console.log(middle([6,2,3,4,9,6,1,0,5])); // => [9]
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => [])
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
