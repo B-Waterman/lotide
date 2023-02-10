faec4aaa61e7042ac901845516b6c43962da22e2
B-Waterman <brooke.waterman@outlook.com>
2023-02-10 06:01:32 -0800

    Completed middle function for all three possible array inputs. Removed test cases and un-needed comments.

--- middle.js
+++ middle.js
@@ -33,7 +33,16 @@ const assertEqual = function(actual, expected) {
 //Scenario 3: array with only one or two elements total, return empty array
 const middle = function(array) {
   let newArray = []
-  
+  if (array.length <= 2) {
+    return newArray;
+  } else if (array.length % 2 === 0) {
+    let elementOne = ((array.length / 2) - 1); 
+    let elementTwo = ((array.length / 2));
+    newArray.push(array[elementOne], array[elementTwo]);
+    return newArray;
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
 };
 
 //Test Cases:
