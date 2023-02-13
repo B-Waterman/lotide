15ddf31aff13cc421039c5ce6cd63afcb7c1740d
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 05:38:07 -0800

    Created findKeyByValue function, included assertEqual function for testing, test cases made and ready. Continuing with implementation

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  //Template literals pass the assertion's actual values into the final string!
+};
+
+//Test Code below
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('Hello World', 'Hello World');
+assertEqual('cherry', 10);
+
+const findKeyByValue = function(bestTVShowsByGenre, showName) {
+
+};
+
+//Test Case:
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
