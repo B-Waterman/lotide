0ec79267cbb0265d1cf06f2d9226a770452fe97b
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 12:12:02 -0800

    Created findKey function, included aassertEqual function. Brought over original findKeyByValue function as framework. Work in progress.

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  //Template literals pass the assertion's actual values into the final string!
+};
+
+const findKey = function(object, callback) {
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, i => i.stars === 2) // => "noma"
