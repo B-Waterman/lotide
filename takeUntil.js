ee6a7cda362d5331951a1cf634bbaf4b9eb1b336
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 09:24:34 -0800

    Added test case composed of array and variable calling the callback function. Console.log prints [function] to screen # of times equivalent to items in array. Work in progress.

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,21 @@
 //Returns a slice of the array w/elements taken from the beginning
 //Continues until callback returns truthy value
+//what creates a truthy value? Do i need that goal or just the framework like map.js?
 //So we know we're working with booleans and likely .slice() or .splice()
-const takeUntil = function(array, callback) {
 
-}
+//Test Input
+array1 = [1, 2, 3, 4, 5, 60, 700, 8200]
+
+const takeUntil = function(array, callback) {
+  let slicedArray = [];
+  for (let item of array) {
+    if (callback(item) !== true) {
+    }
+    console.log(callback)
+  }
+  return slicedArray;
+};
+
+// Test Case:
+const newArray = takeUntil(array1, x => x < 0);
+console.log(newArray);
