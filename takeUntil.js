d88ada9684819bacaba08b0f0761446dfc900c0b
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 09:00:26 -0800

    Created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,6 @@
+//Returns a slice of the array w/elements taken from the beginning
+//Continues until callback returns truthy value
+//So we know we're working with booleans and likely .slice() or .splice()
+const takeUntil = function(array, callback) {
+
+}
