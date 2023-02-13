987329e8763ed28c1d44b0c41404bf503eb932f6
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 11:59:18 -0800

    First return slicedArray wasn't properly scoped, fixed. Removed planning comments & added eqArrays, assertArraysEqual, & another test case.

--- takeUntil.js
+++ takeUntil.js
@@ -3,19 +3,19 @@
 //what creates a truthy value? Do i need that goal or just the framework like map.js?
 //So we know we're working with booleans and likely .slice() or .splice()
 
-//Test Input
-array1 = [1, 2, 3, 4, 5, 60, -700, 8200]
 
 const takeUntil = function(array, callback) {
   let slicedArray = [];
   for (let i of array) {
     if (callback(array[i])) {
       slicedArray = array.slice(0, i)
-    } return slicedArray;
+      return slicedArray;
+    }
   }
   return slicedArray;
 };
 
 // Test Case:
+const array1 = [1, 2, 3, 4, -5, 60, -700, 8200]
 const newArray = takeUntil(array1, i => i < 0);
-console.log(newArray);
+console.log(newArray); //expected log: [1, 2, 3, 4]
