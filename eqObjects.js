1070e5c871df6207ba656b1db78f7cdd23a24812
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 06:56:04 -0800

    One assertion passed out of two. Re-ordered eqArrays and assertEqual functions, and condensed first two falsey statements into one.

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-  //Template literals pass the assertion's actual values into the final string!
-};
-
 const eqArrays = function(arr1, arr2) {
   // takes two arrays, if equal = true, if not = false
   if (arr1.length !== arr2.length) {
@@ -28,26 +19,39 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  //Template literals pass the assertion's actual values into the final string!
+};
+
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-//Goal: function can assess both the length and the content of each object's array and return false if two different objects have the same properties, or same properties but different order, one more or less than the other - Anything besides a Perfect Match
+//Goal: function can assess both the length and the content of each object's array, return false if object has one more or less key than the other - Anything besides a Perfect Match
 // similar to eqArrays
 
 //Test if both objects are arrays, if length of array is same, if the keys And their values match
 const eqObjects = function(object1, object2) {
-  if (object1.length !== object2.length) {
-    return false
-  }
-  if (Object.keys(object1) !== Object.keys(object2)) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false
   } else {
     for (let i = 0; i < object1.length; i++) {
       if (Array.isArray(object1[i]) && Array.isArray(object2[i])) {
         if (eqArrays(object1[i], object2[i]) === false) {
-
+          return false;
+        }
+      } else {
+        if (object1[i] !== object2[i]) {
+          return false
         }
       }
     }
+    return true;
   }
 
 };
