000e3a62f6dbe0bc06e209dc9bd473ecc8d513a6
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 06:43:06 -0800

    Included eqArrays function to call on it within if statement (if both objects contain an array, but arrays are not equal, return false). May remove Array.isArray expressions on line 45 if unnecessary with inclusion of eqArrays.

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,27 @@ const assertEqual = function(actual, expected) {
   //Template literals pass the assertion's actual values into the final string!
 };
 
+const eqArrays = function(arr1, arr2) {
+  // takes two arrays, if equal = true, if not = false
+  if (arr1.length !== arr2.length) {
+    return false;
+    // checks length first to rule out longer arrays with same starting values
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // if both are arrays, test if arrays are equal
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+      // if elements of arrays are not equal, return false
+    }
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 //Goal: function can assess both the length and the content of each object's array and return false if two different objects have the same properties, or same properties but different order, one more or less than the other - Anything besides a Perfect Match
@@ -19,8 +40,16 @@ const eqObjects = function(object1, object2) {
   }
   if (Object.keys(object1) !== Object.keys(object2)) {
     return false
+  } else {
+    for (let i = 0; i < object1.length; i++) {
+      if (Array.isArray(object1[i]) && Array.isArray(object2[i])) {
+        if (eqArrays(object1[i], object2[i]) === false) {
+
+        }
+      }
+    }
   }
-  
+
 };
 
 // Test Case:
