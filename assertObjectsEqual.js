d27af8f733ce2bc4166a1224652f845b2cdc7be4
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 07:08:40 -0800

    Created assertObjectsEqual function, almost exactly assertArraysEqual, changed arr1/arr2 values for object1/object2. Included whole of eqObjects function.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,62 @@
+const eqArrays = function(arr1, arr2) {
+  // takes two arrays, if equal = true, if not = false
+  if (arr1.length !== arr2.length) {
+    return false;
+    // checks length first to rule out longer arrays with same starting values
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // if both are arrays, test if arrays are equal
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+      // if elements of arrays are not equal, return false
+    }
+  }
+  return true;
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+  //Template literals pass the assertion's actual values into the final string!
+};
+
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let i = 0; i < object1.length; i++) {
+      if ((Array.isArray(object1[i]) && Array.isArray(object2[i]))) {
+        if (eqArrays(object1[i], object2[i]) === false) {
+          return false;
+        }
+      } else {
+        if (object1[i] !== object2[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  if (eqArrays(object1, object2) === true) {
+    console.log(`✅✅✅Assertion Passed: ${object1} === ${object2}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${object1} !== ${object2}`);
+  }
+};
