f93463e119394aad83631390328efde1b56104e3
B-Waterman <brooke.waterman@outlook.com>
2023-02-13 06:10:55 -0800

    Created assertEqual test case, eqObjects not yet implemented: both tests failed as intended.

--- eqObjects.js
+++ eqObjects.js
@@ -10,5 +10,14 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  
+
 };
+
+// Test Case:
+
+const shirtObject = {colour: "red", size: "medium"};
+const anotherCoolerShirtObject = {size: "medium", colour: "red"};
+assertEqual(eqObjects(shirtObject, anotherCoolerShirtObject), true); //Should fail!
+
+const longSleeveShirtObject = {size: "medium", colour: "red", sleaveLength: "long"};
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); //Should fail!
