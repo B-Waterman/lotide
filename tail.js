9e70e5b8ef23900faba166d2a332eb0cb89ae432
B-Waterman <brooke.waterman@outlook.com>
2023-02-15 08:33:13 -0800

    Refactored tail to export function and require assertEqual

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-//Determines if two parameters are equal in type and value
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   let newArray = array.slice(1, array.length);
@@ -13,15 +6,4 @@ const tail = function(array) {
 };
 //Should return all elements of an array except the first, AKA the tail
 
-//Test Case 1: Check original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-//Test Case 2: Array with 1 element
-const lonelyNumber = [1];
-console.log(tail(lonelyNumber)); //Should yield an empty array as its tail; it does!
-
-//Test Case 3: Empty array
-const barrenArray = [];
-console.log(tail(barrenArray)); //Should yield an empty array as its tail; it does!
+module.exports = tail;
